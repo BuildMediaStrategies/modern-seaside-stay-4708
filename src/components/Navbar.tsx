91c9187013bfc202332206bfce67dbad2652cb87
BuildMediaStrategies <buildmediastrategies@outlook.com>
2025-08-08 12:12:33 +0100

    Update Logo Size and Add Object-Contain

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -41,7 +41,7 @@ export default function Navbar() {
             <img 
               src="/assets/ChatGPT Image Aug 8, 2025, 11_43_00 AM.png" 
               alt="Cure Cancer @ UCL Logo" 
-              className="h-16 w-auto"
+             className="h-20 w-auto object-contain"
             />
           </Link>
         </div>
